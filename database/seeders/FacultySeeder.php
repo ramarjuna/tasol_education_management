40fd27d8a290106ce2ae0e817921584a1fff8643
ramarjuna <ramarjuna.07@gmail.com>
2022-05-11 23:23:41 +0530

    Factories and seeders added for Faculty and Subject

---
+++ database/seeders/FacultySeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Faculty;
+
+class FacultySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      Faculty::factory()->count(15)->create();
+    }
+}
